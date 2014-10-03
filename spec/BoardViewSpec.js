d2f5aa42f1e4ccd8d66cc43e8d07f3597e0b888a
Austen Talbot <austen@asana.com>
2014-10-02 17:33:18 -0700

    Added sandbox div to display dom changes along with tests

--- spec/BoardViewSpec.js
+++ spec/BoardViewSpec.js
@@ -8,4 +8,29 @@ describe('ChessboardView', function() {
   it('should exist', function() {
     expect(view).to.be.ok;
   });
+
+  // This function uses a board visualizer lets you view an interactive version of any piece matrix.
+  var displayBoard = function(matrix){
+    $('#sandbox').html(
+      new BoardView({
+        model: new Board(matrix)
+      }).render()
+    );
+  };
+  $(function(){
+    // this is a pre-baked solution for n-queens in the n = 4 case, rendered to the screen using our handy visualizer.
+    displayBoard([
+      [0, 1, 0, 0],
+      [0, 0, 0, 1],
+      [1, 0, 0, 0],
+      [0, 0, 1, 0]
+    ]);
+
+    var n = 4;
+    findNRooksSolution(n);
+    countNRooksSolutions(n);
+    findNQueensSolution(n);
+    countNQueensSolutions(n);
+  });
+
 });
